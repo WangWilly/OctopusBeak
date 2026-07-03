56eb8185ad363bb50e2b46f3ffd53d86021c38ce
WangWilly <s89667654@gmail.com>
2026-07-03 15:09:11 +0800

    feat: add desktop titlebar safe area

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import "../app.css";
+
+  onMount(() => {
+    if ("octopusBeak" in window) document.documentElement.classList.add("desktop-shell");
+  });
 </script>
 
 <slot />
