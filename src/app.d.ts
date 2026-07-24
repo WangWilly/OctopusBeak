bbbbef42537e5471bc886abf7781244f9503ee9b
WangWilly <s89667654@gmail.com>
2026-07-24 18:35:51 +0800

    Add Svelte typings for onboarding events

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,6 +4,12 @@ declare global {
   interface Window {
     octopusBeak: OctopusBeakApi;
   }
+  namespace svelteHTML {
+    interface HTMLAttributes<T> {
+      ononboardingadvance?: (event: CustomEvent<void>) => void;
+      ononboardingback?: (event: CustomEvent<void>) => void;
+    }
+  }
 }
 
 export {};
